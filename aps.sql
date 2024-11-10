ce18b320cc5c5f9a5b3d2b2240b94f7d5b8f9aa4
Italoomoura <italomourasant9@gmail.com>
2024-11-10 20:29:38 -0300

    add CRUD

--- aps.sql
+++ aps.sql
@@ -1,12 +1,11 @@
-
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 14/10/2024 às 23:05
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+-- Tempo de geração: 11-Nov-2024 às 00:25
+-- Versão do servidor: 10.4.17-MariaDB
+-- versão do PHP: 8.0.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -25,62 +24,66 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `login`
+-- Estrutura da tabela `noticias`
 --
 
-CREATE TABLE `login` (
-  `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `noticias` (
+  `titulo` varchar(255) NOT NULL,
+  `conteudo` text NOT NULL,
+  `data_criacao` timestamp NOT NULL DEFAULT current_timestamp(),
+  `id` int(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `noticias`
+--
+
+INSERT INTO `noticias` (`titulo`, `conteudo`, `data_criacao`, `id`) VALUES
+('Teste', 'teste teste', '2024-11-10 23:16:38', 1);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `person`
+-- Estrutura da tabela `person`
 --
 
 CREATE TABLE `person` (
-  `id` int(80) NOT NULL,
   `first_name` varchar(80) NOT NULL,
   `last_name` varchar(80) NOT NULL,
   `office` varchar(80) NOT NULL,
-  `nasc_date` varchar(80) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `nasc_date` varchar(80) NOT NULL,
+  `id` int(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Despejando dados para a tabela `person`
+-- Extraindo dados da tabela `person`
 --
-
-INSERT INTO `person` (`id`, `first_name`, `last_name`, `office`, `nasc_date`) VALUES
-(1, 'Italo', 'Moura', 'Developer', '14/09/2004'),
-(2, 'teste', 'testando', 'adm', '12/08/2007');
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `login`
+-- Índices para tabela `noticias`
 --
-ALTER TABLE `login`
+ALTER TABLE `noticias`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices de tabela `person`
+-- Índices para tabela `person`
 --
 ALTER TABLE `person`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT para tabelas despejadas
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT de tabela `login`
+-- AUTO_INCREMENT de tabela `noticias`
 --
-ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `noticias`
+  MODIFY `id` int(80) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `person`
